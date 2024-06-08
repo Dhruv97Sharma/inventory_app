1f5ed46325cdfce89f1c9244e00705514691cfea
Dhruv Sharma <dhruv97sharma@Dhruvs-MacBook-Air.local>
2024-06-09 01:26:39 +0530

    Added category and item model in inventory app

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    number_in_stock = models.IntegerField(default=0)
+    description = models.TextField()
+    url = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    url = models.TextField()
+    items = models.ManyToManyField(Item)
+
+    def __str__(self):
+        return self.name
